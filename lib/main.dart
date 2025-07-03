71b3de3b560c30a9384760a44736bba730edfb92
mbk97 <oyindamola850@gmail.com>
2025-07-03 08:04:14 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:workout_mobile_app/screens/welcome.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Welcome(),
   ));
